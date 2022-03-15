889fe78c08900914ad4284092f82260241d4270a
thisLife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-03-15 15:41:50 +0900

    change color

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const myHeading = document.querySelector("h1");
 
-myHeading.textContent = "Hello World 안녕하슈";
+// myHeading.textContent = "Hello World 안녕하슈";
+myHeading.textContent = "<h2>졸려</h2>";
+
+// document.body.innerHTML = "<h2>졸려</h2>";
